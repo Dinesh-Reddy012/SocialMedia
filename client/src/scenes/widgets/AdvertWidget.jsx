3bc3b2627af4056356a009be65c7a31f9a2f6e9d
DINESH REDDY <dineshreddy8844@gmail.com>
2025-06-23 23:18:49 +0530

    Update AdvertWidget.jsx

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="https://socialmedia-app-97st.onrender.com/assets/info4.jpeg"
+        src="https://socialmedia-backend-wsi0.onrender.com/assets/info4.jpeg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
