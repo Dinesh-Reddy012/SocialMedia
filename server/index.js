202eab0c8f0839d3bfa79491bd051ca3a93f727f
DINESH REDDY <dineshreddy8844@gmail.com>
2025-07-11 16:13:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ import { users,posts} from "./data/index.js"
 // Configurations
 const __dirname = path.resolve()
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 dotenv.config();
 const app=express();
 app.use(express.json())
