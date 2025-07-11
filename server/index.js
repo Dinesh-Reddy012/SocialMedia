d7df9a64f5866c9d64f57898af7f92df8280ad76
DINESH REDDY <dineshreddy8844@gmail.com>
2025-07-11 16:02:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,9 +17,11 @@ import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js"
 import Post from "./models/post.js"
 import { users,posts} from "./data/index.js"
+import path from 'path';
+
 
 // Configurations
-
+const __dirname = path.resolve()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 dotenv.config();
@@ -32,6 +34,7 @@ app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit : "30mb", extended: true}))
 app.use(cors())
 app.use("/assets",express.static(path.join(__dirname,"public/assets")))
+
 
 // FILE STORAGE
 
